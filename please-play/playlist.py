c93346b1bc137f03fde5c98fd6db6e904d4d95c9
Olav Kaada <olav.kaada@gmail.com>
2017-12-06 01:17:08 +0000

    File not found error handling

--- please-play/playlist.py
+++ please-play/playlist.py
@@ -1,22 +1,30 @@
 import re
 import codecs
-
+import errno
 
 def fetch_songs(file_path):
-    with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+    try:
+        with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
 
-        if file_path.endswith('.m3u'):
-            if not 'EXTM3U' in f.readline():
-                print('Error: The .m3u-file has no song information (EXTINF-fields are missing)')
-                exit(1)
-            reg = re.compile('#EXTINF:\d+,([^\r\n]+)')
+            if file_path.endswith('.m3u'):
+                if not 'EXTM3U' in f.readline():
+                    print('Error: The .m3u-file has no song information (EXTINF-fields are missing)')
+                    exit(1)
+                reg = re.compile('#EXTINF:\d+,([^\r\n]+)')
+            else:
+                reg = re.compile('([^\r\n]+)')
+
+            songs = []
+            for line in f:
+                match = re.search(reg, line)
+                if match:
+                    songs.append(match.group(1))
+
+            return songs
+
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print("Error: File '{}' not found".format(e.filename))
         else:
-            reg = re.compile('([^\r\n]+)')
-
-        songs = []
-        for line in f:
-            match = re.search(reg, line)
-            if match:
-                songs.append(match.group(1))
-
-        return songs
+            print(e)
+        exit(1)
