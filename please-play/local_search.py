2fad27dce01b974d54c66c2340076267906c6e91
Olav Kaada <olav.kaada@gmail.com>
2018-08-27 19:51:00 +0200

    Wip for searching local files

--- please-play/local_search.py
+++ please-play/local_search.py
@@ -2,12 +2,12 @@ import subprocess
 import fnmatch
 import os
 
-BASE_PATH = '/Users/okaada/Music/flac/'
+BASE_PATH = '/Users/USERNAME/Music/'
 
 r = []
 for root, _, files in os.walk(BASE_PATH, topdown=False):
     for name in files:
-        if fnmatch.fnmatch(name, '*ABBA*.flac'):
+        if fnmatch.fnmatch(name, '*SONG NAME*.flac'):
             r.append(os.path.join(root, name))
-print(r)
+
 subprocess.call(['mpv', *r])
